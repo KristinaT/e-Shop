5646d572560ccfc7d51f20f57a2904d12499a1cb
Kristina Taneva <tinatanev@gmail.com>
2019-08-04 15:57:48 +0200

    feat: added header and home, shop, contact links

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/Homepage/HomePage';
 import ShopPage from './pages/Shop/ShopPage';
+import Header from './components/Header/Header';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
