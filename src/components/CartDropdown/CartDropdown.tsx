962c7be5706b8adbe2e99b8f68717843b2de677e
Kristina Taneva <tinatanev@gmail.com>
2019-08-31 19:03:34 +0200

    feat: cart items in basket done

--- src/components/CartDropdown/CartDropdown.tsx
+++ src/components/CartDropdown/CartDropdown.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import CustomButton from '../CustomButton/CustomButton';
 import './CartDropdown.scss';
+import { connect, MapStateToProps } from 'react-redux';
+import { RootState } from '../../redux/reducers/types';
+import CartItem from '../CartItem/CartItem';
 
-const CartDropdown: React.FC<{}> = () => (
+interface OwnProps {}
+interface StateProps {
+  cartItems: any[];
+}
+interface DispatchProps {}
+
+type Props = OwnProps & StateProps & DispatchProps;
+
+const CartDropdown: React.FC<Props> = ({ cartItems }) => (
   <div className='cart-dropdown'>
-    <div className='cart-items' />
+    <div className='cart-items'>
+      {cartItems.map(cartItem => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
-export default CartDropdown;
+const mapStateToProps: MapStateToProps<
+  StateProps,
+  OwnProps,
+  RootState
+> = state => ({
+  cartItems: state.cart.cartItems
+});
+
+export default connect(mapStateToProps)(CartDropdown);
---
+++ src/components/CartItem/CartItem.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './CartItem.scss';
+interface OwnProps {
+  item: {
+    imageUrl: string;
+    price: number;
+    name: string;
+    quantity: number;
+  };
+}
+interface StateProps {}
+interface DispatchProps {}
+
+type Props = OwnProps & StateProps & DispatchProps;
+
+const CartItem: React.FC<Props> = ({
+  item: { imageUrl, price, name, quantity }
+}) => (
+  <div className='cart-item'>
+    <img src={imageUrl} alt='item' />
+    <div className='item-details'>
+      <span className='name'>{name}</span>
+      <span className='price'>
+        {quantity} x ${price}
+      </span>
+    </div>
+  </div>
+);
+
+export default CartItem;
